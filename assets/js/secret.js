4630c0f2ce1177484c89ddb06142447b93fc14cc
theLittleBigZ <zeeshan.badr@mail.utoronto.ca>
2020-05-26 01:51:14 -0400

    added secret.js

---
+++ assets/js/secret.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCpeW7zSFbo1UmM0d0iARIC4jY9Jp5w-j8",
+    authDomain: "the403-e7189.firebaseapp.com",
+    databaseURL: "https://the403-e7189.firebaseio.com",
+    projectId: "the403-e7189",
+    storageBucket: "the403-e7189.appspot.com",
+    messagingSenderId: "975648852002",
+    appId: "1:975648852002:web:a42fac244478c4cca6627b"
+};
